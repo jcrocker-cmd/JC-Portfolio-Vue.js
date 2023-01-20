8403b299dcd2198b6feb783208a4739b7d65541c
John Christian B. Narbaja <narbajajc@gmail.com>
2023-01-20 21:16:41 +0800

    First Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <Navbar/>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+
+export default{
+  name: 'Home',
+  components: {
+    Navbar
+    
+  },
+}
+</script>
+
+<style>
+
+</style>
